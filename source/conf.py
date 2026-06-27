52121fe82d10237adaacd0728a6588cebd128394
dawszo21 <DawDan2103@gmail.com>
2026-06-27 14:35:21 +0200

    Dodanie i naprawa PDF

--- source/conf.py
+++ source/conf.py
@@ -2,18 +2,23 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('rozdzial_5/BD_Zapytania'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sprawozdanie z labow'
-copyright = '2026, dawid'
-author = 'dawid'
+project = 'Sprawozdanie z laboratorium: Bazy Danych'
+copyright = '2026, Dawid Szokało'
+author = 'Dawid Szokało'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,3 +30,7 @@ language = 'pl'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+latex_elements = {
+    'extraclassoptions': 'openany,oneside',
+}
